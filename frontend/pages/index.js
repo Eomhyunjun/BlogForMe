9476906debf543dd9114a382940804707908268f
Eomhyunjun <zzp1318@gmail.com>
2022-08-14 22:32:04 +0900

    init project: with emotion/css

---
+++ frontend/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Heom 블로그</title>
+      </Head>
+      기본설정입니다
+    </div>
+  )
+}
